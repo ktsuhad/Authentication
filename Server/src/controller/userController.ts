0f18dd834af3f98e9727ed2bc383dfdad43ed9bd
SUHAD.KT <suhadkizhakethodi@gmail.com>
2023-10-01 12:21:41 +0530

    added gitignore

--- Server/src/controller/userController.ts
+++ Server/src/controller/userController.ts
@@ -4,8 +4,7 @@ import { userModel } from "../model/userModel";
 export const signupController = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
-    //find existingUser
-    const existingUser =await userModel.findOne({ email });
+    const existingUser =await userModel.findOne({ email });       //find existingUser
 
     if(existingUser) {
       return res.status(400).json({ succes: false, message: "user already exist" });
@@ -13,7 +12,11 @@ export const signupController = async (req: Request, res: Response) => {
 
     const user = new userModel({ name, email, password });
     await user.save();
-  } catch (error) {}
+    return res.status(201).json({ succes: true, message: "user created successfully" });
+
+  } catch (error) {
+    return res.status(500).json({ succes: false, message: "error while creating user",error });
+  }
 };
 
 export const loginController = async (req: Request, res: Response) => {}
